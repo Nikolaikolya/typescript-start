05206676e00bae86995de5ea035f609d04a7ec34
Mikolai Fedotov <starcrafters@ya.ru>
2021-05-29 23:26:09 +0700

    Base type in TypeScript and type props into function

--- lesson_1/index.ts
+++ lesson_1/index.ts
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// Inteface служат для описания объекта (какие сво-ва будут находиться в нем)
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
 axios.get(url).then(response => {
-	console.log(response.data);
+	const { id, title, completed } = response.data as Todo;
+
+	logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+	console.log(id, title, completed );
+}
