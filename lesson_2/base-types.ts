773392c97ec0834e0976d0ba34b429b9a7e616e5
Mikolai Fedotov <starcrafters@ya.ru>
2021-05-30 11:32:24 +0700

    Add lesson 2 information

---
+++ lesson_2/base-types.ts
@@ -0,0 +1,15 @@
+const number: number = 0;
+
+const today: Date = new Date();
+
+console.log(today.getDate());
+
+
+const string: string = 'Hello';
+
+console.log(number, string);
+
+
+const person = {
+	age: 21
+}
